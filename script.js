65f9f9866fb8da39203ff66912a1da1aa7c8c1b3
IanWang0410 <145990493+IanWang0410@users.noreply.github.com>
2024-06-03 03:35:34 +0800

    noti scope

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('service-worker.js', {scope : '/'})
     .then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch(function(error) {
