e8218c452d7235fc1acabee5c32d9aaf2fb0a812
IanWang0410 <145990493+IanWang0410@users.noreply.github.com>
2024-06-03 21:24:07 +0800

    noti test

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    // Handle the notification click event
+
     event.waitUntil(
       clients.openWindow('https://ianwang0410.github.io/')
     );
